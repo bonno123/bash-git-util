e4b3505ae433b80e1314bf8ef2cb18218df055b8
bonno123 <banikavik3@gmail.com>
2023-07-13 04:04:12 +0530

    fix: terminal exit on ctrl+c

--- bash_dropdown.sh
+++ bash_dropdown.sh
@@ -2,8 +2,8 @@
 
 # following: https://unix.stackexchange.com/questions/146570/arrow-key-enter-menu
 
-# Renders a text based list of options that can be selected by the
-# user using up, down and enter keys and returns the chosen option.
+# Renders a text based list of options that can be selected by the user 
+# using up, down and enter keys and returns the chosen option.
 
 ESC=$( printf "\033")
 
@@ -16,12 +16,13 @@ dim_text_normal_underline_blue=$ESC'[1;2;4;94m'
 
 selection_arrow='\u27A6'
 
-# little helpers for terminal print control and key input
+# helpers related to cursor
 function cursor_blink_on  { printf "$ESC[?25h"; }
 function cursor_blink_off { printf "$ESC[?25l"; }
 
 function cursor_to { printf "$ESC[$1;${2:-1}H"; }
 
+# helpers for print format
 function print_option { printf "   $bold_italic_blue$1  $reset_format  "; }
 function print_selected { 
     printf " $bold_normal_blue$selection_arrow $dim_text_normal_underline_blue$1$reset_format "; 
@@ -31,10 +32,10 @@ function print_selected {
 
 function get_cursor_row { IFS=';' read -sdR -p $'\E[6n' ROW COL; echo ${ROW#*[}; }
 
-# function trap_ctrlc() {
-#     echo -e "\n\ntrap_ctrlc\n\n"
-#     trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
-# }
+function trap_ctrlc() {
+    # echo -e "\n\ntrap_ctrlc\n"
+    trap "cursor_blink_on; stty echo; printf '\n';return 255" 2
+}
 
 #   Arguments   : list of options, maximum of 256 ("opt1" "opt2" ...)
 #   Return value: selected index (0 for opt1, 1 for opt2 ...)
@@ -49,7 +50,8 @@ function select_option {
     local startrow=$(($lastrow - $#))
 
     # ensure cursor and input echoing back on upon a ctrl+c during read -s
-    trap "cursor_blink_on; stty echo; printf '\n'; exit" 2
+    # trap "cursor_blink_on; stty echo; printf '\n'; exit" 1
+    trap_ctrlc
     cursor_blink_off
 
     local selected=0
@@ -69,11 +71,15 @@ function select_option {
 
         # user key control
         case `key_input` in
-            enter) break;;
-            up)   ((selected--));
-                   if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
-            down)  ((selected++));
-                   if [ $selected -ge $# ]; then selected=0; fi;;
+            enter)  break;;
+
+               up)  ((selected--));
+                    if [ $selected -lt 0 ]; then selected=$(($# - 1)); fi;;
+
+             down)  ((selected++));
+                    if [ $selected -ge $# ]; then selected=0; fi;;
+
+             quit)  return 255;;
         esac
     done
 
@@ -92,11 +98,12 @@ function key_input {
         echo up;    
     fi;
 
-    if [[ $key = $ESC[B ]]; 
-    then echo down;  
+    if [[ $key = $ESC[B ]]; then 
+        echo down;  
     fi;
 
-    if [[ $key = "" ]]; 
-    then echo enter; 
+    if [[ $key = "" ]]; then 
+        echo enter; 
     fi; 
+
 }
